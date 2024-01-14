c9e685238af8e9f23f7f3ebc3433e7097b3926d9
greenera <ivan@drinks.group>
2024-01-14 11:54:04 +0200

    feat: add upgrade data

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -2,13 +2,13 @@
 
 namespace EPuzzle\CustomerPrice\Setup;
 
-use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
 
-class InstallData implements InstallDataInterface
+class UpgradeData implements UpgradeDataInterface
 {
-    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         if (version_compare($context->getVersion(), '0.0.1', '<')) {
             $setup->getConnection()->query("create table if not exists epuzzle_customer_price
